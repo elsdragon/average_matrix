4105aaa5fe1fb42709ce1150c7bf6a20a3e29cf8
Lucia Dominguez Bermejo <la_els@hotmail.com>
2023-11-09 22:31:27 +0100

    version 2.0

--- average_matrix.py
+++ average_matrix.py
@@ -67,3 +67,22 @@ def medium_of_list(list):
             divisor -= 1
     
     return total/divisor
+
+
+def matriz_promedio(matrix):
+
+    ''' 
+    Change all elements of a matrix by aritmetic average itself and its neighbours. 
+    '''
+
+    matrix_none = add_none_matrix(matrix)
+    neighbour_matrix = add_list_neighbour(matrix_none)
+    average_matrix = []
+    for i in range(len(neighbour_matrix)):
+        average_list = []
+        for lista in neighbour_matrix[i]:
+            average_list.append(medium_of_list(lista))
+        average_matrix.append(average_list)
+    
+    
+    return average_matrix
--- average_matrix_test.py
+++ average_matrix_test.py
@@ -25,4 +25,11 @@ def test_add_list_neighbour():
 def test_medium_of_list():
 
     assert medium_of_list([1, 2, 3]) == 2
+
+
+def test_matriz_promedio():
+
+    assert matriz_promedio([[1,2, 3],[2,1, 3], [3, 2, 1]]) == [[1.6666666666666667, 1.75, 2.6666666666666665],
+                                     [1.75, 2.0, 2.0],
+                                     [2.3333333333333335, 1.75, 2.0]]
     
